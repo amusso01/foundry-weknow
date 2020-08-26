e8350485f637a50ec581cc2a7570e4f347ce9b86
amusso01 <eandram@hotmail.it>
2020-08-26 17:15:36 +0100

    merging

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ docReady(function() {
   // MENU
   var menuButton = document.getElementById("book-menu-button");
   var menu = document.getElementById("book-menu");
+  var menuMobile = document.getElementById("book-menu-mobile");
+  var hamburger = document.getElementById("hamburger-6");
+  var htmlElement = document.querySelector("html");
   menuButton.addEventListener("click", function() {
     menu.classList.toggle("active");
+  });
+  hamburger.addEventListener("click", function(e) {
+    hamburger.classList.toggle("is-active");
+    menuMobile.classList.toggle("active");
+    // prevent content scrolling
+    htmlElement.classList.toggle("noscroll");
   });
 });
